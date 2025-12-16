acf820ef5c82bb2ccc8d864e5ab57b344d3d4088
SimonAndersson0 <151626143+SimonAndersson0@users.noreply.github.com>
2025-12-16 15:23:36 +0100

    made cards nearly funnctional

--- Monopoly/CardDeck.cpp
+++ Monopoly/CardDeck.cpp
@@ -1,1 +1,43 @@
 #include "CardDeck.h"
+#include "Card.h"
+#include <algorithm>
+
+
+
+
+
+CardDeck::CardDeck(const std::string& xmlFilePath)
+    : m_rng(std::random_device{}()) // seed RNG
+{
+    loadFromXML(xmlFilePath);       // load cards
+    shuffle();                      // shuffle deck immediately
+}
+
+Card CardDeck::draw()
+{
+    if (m_cards.empty())
+        throw std::runtime_error("Deck is empty");
+
+    Card card = m_cards.back();
+    m_cards.pop_back();
+    return card;
+}
+
+
+void CardDeck::returnCard(const Card& card)
+{
+    m_cards.insert(m_cards.begin(), card); // bottom of deck
+}
+
+void CardDeck::shuffle()
+{
+    std::shuffle(m_cards.begin(), m_cards.end(), m_rng);
+}
+
+
+
+void CardDeck::loadFromXML(const std::string& xmlFilePath)
+{
+
+}
+
--- Monopoly/CardDeck.h
+++ Monopoly/CardDeck.h
@@ -2,14 +2,25 @@
 
 #include <string>
 #include <vector>
+#include <random>
 
 class Card;
 
 class CardDeck
 {
 	public:
-		CardDeck();
+		CardDeck(const std::string& xmlFilePath); //initialize an deck
+
+		Card draw();
+		void returnCard(const Card& card);//returnCard() takes a reference to avoid copying twice
+		void shuffle();
+
 	private:
-	std::vector<Card*> m_properties;
+		std::vector<Card> m_cards;
+		std::mt19937 m_rng; // Random number generator for shuffling
+
+		// Helper to load XML
+		void loadFromXML(const std::string& xmlFilePath);
+
 };
 
